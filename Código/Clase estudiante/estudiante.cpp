d976362116abe8acdd43abbe289f5063daf13906
LeoNarvaez2004 <leonardo_8.8@live.com>
2024-06-12 20:51:50 -0500

    Clase estudiante xd

---
+++ Código/Clase estudiante/estudiante.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Estudiante{
+    private:
+        string usuario;
+        string contrasenia;
+    public:
+        Estudiante (string _usuario=" ", string _contrasenia=" ") : usuario(_usuario),contrasenia(_contrasenia){}
+    void ingresarDatos() {
+        cout<<"Ingrese el usuario: ";
+        getline(cin,usuario);
+        cout<<"Ingrese la contrasenia: ";
+        getline(cin,contrasenia);
+    }
+    void mostrarDatos(){
+        cout<<"Usuario: "<<usuario<<endl;
+        cout<<"Contrasenia: "<<contrasenia<<endl;
+    }
+};
+int main(){
+    Estudiante cliente;
+    cliente.ingresarDatos();
+    cliente.mostrarDatos();
+}
+
+
