987a74b1f5dc9096808f16fb1bdc2203eaa5e875
DeniseMimi <noemiyoto@hotmail.com>
2024-06-14 19:03:09 -0500

    Corrección Menu

--- Código/Menu.h
+++ Código/Menu.h
@@ -16,10 +16,15 @@ int Menu::obtenerIS_CC(){
     int cc=0;
     IngresoDatos i;
     do{
-
+    cout<<"BIENVENIDOS A ESPExpress"<<endl;
+    cout<<"Usted desea..."<<endl;
+    cout<<"1. Iniciar Sesion"<<endl;
+    cout<<"2. Crear Cuenta"<<endl;
+    cc=i.IngresoEnteros("Opcion>>");
     }while(cc<1||cc>2);
+    return cc;
 }
 
 bool Menu::MenuLogin(){
-
+return false;
 }
