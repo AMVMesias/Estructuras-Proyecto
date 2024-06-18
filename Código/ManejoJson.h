f14c9e85dceafade79ba89147e838479ca0b0226
mesiasv2 <mesiasv2@gmail.com>
2024-06-18 11:38:26 -0500

    Actualizacion BD CI con int

--- Código/ManejoJson.h
+++ Código/ManejoJson.h
@@ -8,11 +8,13 @@
 #include "nlohmann/json.hpp"
 #include "Lista.h"
 #include "Estudiante.h"
+#include "Producto.h"
 
 using namespace std;
 using json = nlohmann::json;
 
 const string CARPETA_USUARIOS = "Usuarios"; 
+const string CARPETA_PRODUCTOS = "Productos";  
 
 bool estudianteExiste(int usuario) {
     string nombreArchivo = CARPETA_USUARIOS + "/usuario_" + to_string(usuario) + ".json";
@@ -29,7 +31,7 @@ void crearNuevoUsuario_BBD(int usuario, const string& contrasenia, const string&
     string nombreArchivo = CARPETA_USUARIOS + "/usuario_" + to_string(usuario) + ".json";
 
     json j;
-    j["ci"] = to_string(usuario);
+    j["ci"] = usuario;
     j["contrasena"] = contrasenia;
     j["nombres"] = nombre;
 
@@ -90,5 +92,41 @@ void cargarEstudiantes(Lista<Estudiante>& listaEstudiantes) {
         cerr << "No se pudo abrir el directorio: " << CARPETA_USUARIOS << endl;
     }
 }
+
+
+bool productoExiste(int id) {
+    string nombreArchivo = CARPETA_PRODUCTOS + "/producto_" + to_string(id) + ".json";
+    ifstream archivo(nombreArchivo);
+    return archivo.good();
+}
+
+void crearNuevoProducto(int id, const string& descripcion, float precio, int cantidad) {
+    if (productoExiste(id)) {
+        cout << "Error: El producto con ID '" << id << "' ya existe." << endl;
+        return;
+    }
+
+    string nombreArchivo = CARPETA_PRODUCTOS + "/producto_" + to_string(id) + ".json";
+
+    json j;
+    j["id"] = id;
+    j["descripcion"] = descripcion;
+    j["precio"] = precio;
+    j["cantidad"] = cantidad;
+
+    ofstream archivo(nombreArchivo);
+    if (archivo.is_open()) {
+        archivo << j.dump(4);
+        archivo.close();
+        cout << "Producto creado exitosamente: " << descripcion << endl;
+    } else {
+        cerr << "No se pudo abrir el archivo para escribir: " << nombreArchivo << endl;
+    }
+}
+
+
+
+
+
 
 #endif 
